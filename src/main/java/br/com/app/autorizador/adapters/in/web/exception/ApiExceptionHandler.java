bc39fae0f7eba766e9e913e4f2bc6fd90377cbc4
yurifws <yurifws@gmail.com>
2023-12-12 23:43:23 -0300

    feat: inclusao de classes base camada adapter web

---
+++ src/main/java/br/com/app/autorizador/adapters/in/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,5 @@
+package br.com.app.autorizador.adapters.in.web.exception;
+
+public class ApiExceptionHandler {
+
+}
---
+++ src/main/java/br/com/app/autorizador/adapters/in/web/transacoes/mapper/TransacaoControllerMapper.java
@@ -0,0 +1,5 @@
+package br.com.app.autorizador.adapters.in.web.transacoes.mapper;
+
+public interface TransacaoControllerMapper {
+
+}
